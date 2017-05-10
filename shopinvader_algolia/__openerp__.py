498943e94ddd0c46bbca98112885bd8253a9c5d9
Sébastien BEAU <sebastien.beau@akretion.com>
2017-05-10 16:57:55 +0200

    [REF] still continuing the refactor make the "shopinvader module" installable

---
+++ shopinvader_algolia/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+{'name': 'Shopinvader Algolia Connector',
+ 'version': '8.0.0.0.1',
+ 'author': 'Akretion',
+ 'website': 'www.akretion.com',
+ 'license': 'AGPL-3',
+ 'category': 'Generic Modules',
+ 'depends': [
+     'shopinvader_search_engine',
+     'connector_algolia',
+ ],
+ 'data': [
+ ],
+ 'demo': [
+ ],
+ 'installable': True,
+ 'application': True,
+ }
