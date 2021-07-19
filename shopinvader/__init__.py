bd9f88b9782e1e3982b30b817bdcb8bef1071064
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-07-19 17:23:35 +0200

    [IMP] shopinvader: Improves service context management and makes security pluggable
    Refactor code to only declare one controller under the root_path '/shopinvader' and makes the authentication mechanism to apply to shopinvader services pluggable.
    The sevice context is now provided by a dedicated component and the specific route used to download invoices is now defined with the new api. Therefore we no more need to define a concrete controller into shopinvader. The concrete controller is now provided by the addon that declare the authentication to apply to services, the root_path where to serve the services and the services.

--- shopinvader/__init__.py
+++ shopinvader/__init__.py
@@ -1,9 +1,4 @@
-# Copyright 2016 Akretion (http://www.akretion.com)
-# Sébastien BEAU <sebastien.beau@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 from . import components
-from . import controllers
 from . import models
 from . import services
 from . import wizards
