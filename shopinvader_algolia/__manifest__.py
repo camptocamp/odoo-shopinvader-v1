ee5c92f59d0b51f842b192dabd9f40e5ff1038f6
Simone Orsi <simahawk@gmail.com>
2025-03-28 10:45:05 +0100

    Fix test dep

--- shopinvader_algolia/__manifest__.py
+++ shopinvader_algolia/__manifest__.py
@@ -11,6 +11,15 @@
     "license": "AGPL-3",
     "category": "Generic Modules",
     "depends": ["shopinvader_search_engine", "connector_algolia"],
+    "external_dependencies": {
+        "python": [
+            # use our own fork of connector_search_engine
+            "odoo-addon-connector_search_engine @ "
+            "git+https://github.com/camptocamp/search-engine-v1@18.0#subdirectory=connector_search_engine",
+            "odoo-addon-connector_algolia @ "
+            "git+https://github.com/camptocamp/search-engine-v1@18.0#subdirectory=connector_algolia",
+        ]
+    },
     "data": [
         "data/ir_export_product.xml",
         "views/shopinvader_variant_view.xml",
