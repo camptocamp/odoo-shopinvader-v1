08a8e59ab947e07f74cd02632999648a6b832df3
Simone Orsi <simahawk@gmail.com>
2025-03-31 15:42:56 +0200

    fixup! shopinvader_delivery_carrier: migrate to v18

--- shopinvader_delivery_carrier/tests/test_notification.py
+++ shopinvader_delivery_carrier/tests/test_notification.py
@@ -1,6 +1,7 @@
 # Copyright 2019 Akretion (http://www.akretion.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from odoo.addons.queue_job.tests.common import trap_jobs
 from odoo.addons.shopinvader.tests.common import CommonCase
 from odoo.addons.shopinvader.tests.test_notification import NotificationCaseMixin
 
@@ -19,11 +20,10 @@ class NotificationPickingCase(CommonCase, NotificationCaseMixin):
         For this case, we should have a notification
         :return:
         """
-        self._init_job_counter()
-        self.picking._action_done()
-        self._check_nbr_job_created(1)
-        self._perform_created_job()
-        self._check_notification("stock_picking_outgoing_validated", self.picking)
+        with self._catpure_notification(
+            "stock_picking_outgoing_validated", notif_target=self.picking
+        ):
+            self.picking._action_done()
 
     def test_picking_notification2(self):
         """
@@ -34,10 +34,10 @@ class NotificationPickingCase(CommonCase, NotificationCaseMixin):
         """
         picking = self.picking.copy()
         # Remove the link with procurement/sales
-        picking.move_lines.write({"sale_line_id": False})
-        self._init_job_counter()
-        picking._action_done()
-        self._check_nbr_job_created(0)
+        picking.move_ids.write({"sale_line_id": False})
+        with trap_jobs() as trap:
+            picking._action_done()
+            trap.assert_jobs_count(0)
 
     def test_picking_notification3(self):
         """
@@ -47,6 +47,6 @@ class NotificationPickingCase(CommonCase, NotificationCaseMixin):
         :return:
         """
         self.picking.picking_type_id.write({"code": "internal"})
-        self._init_job_counter()
-        self.picking._action_done()
-        self._check_nbr_job_created(0)
+        with trap_jobs() as trap:
+            self.picking._action_done()
+            trap.assert_jobs_count(0)
