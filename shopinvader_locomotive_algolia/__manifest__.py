089f06a6efcb48cd33b9600b93e7e6c5b520a7f5
Simone Orsi <simahawk@gmail.com>
2025-04-01 16:07:50 +0200

    shopinvader_locomotive_algolia: migrate to v18

--- shopinvader_locomotive_algolia/__manifest__.py
+++ shopinvader_locomotive_algolia/__manifest__.py
@@ -7,9 +7,9 @@
     "summary": """
         This addons is used to push the initial algolia configuration
         to locomotive""",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Camptcamp SA",
+    "author": "Camptocamp",
     "website": "https://github.com/camptocamp/odoo-shopinvader-v1",
     "depends": ["component", "shopinvader_locomotive", "shopinvader_algolia"],
     "data": ["views/se_backend_algolia.xml"],
