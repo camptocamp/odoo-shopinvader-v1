shopinvader/odoo-shopinvader

[10.0][CHG] being able to make the difference between an update done by the customer and update done internally in Odoo

shopinvader_cart_expiry/tests/test_cart_expiry.py
@@ -17,6 +17,8 @@ class TestCartExpiry(CommonCase):
         super(TestCartExpiry, self).setUp()
         self.sale_obj = self.env["sale.order"]
         self.sale = self.env.ref("shopinvader.sale_order_2")
+        self.sale.write({"last_external_update_date": fields.Datetime.now()})
+        self.so_date = self.sale.last_external_update_date
 
     def test_cart_expiry_scheduler(self):
         """
@@ -34,7 +36,7 @@ class TestCartExpiry(CommonCase):
         return
 
     def test_cart_expiry_cancel(self):
-        so_date = fields.Datetime.from_string(self.sale.write_date)
+        so_date = fields.Datetime.from_string(self.so_date)
         today = fields.Datetime.to_string(so_date + timedelta(hours=5))
         self.backend.write(
             {"cart_expiry_delay": 1, "cart_expiry_policy": "cancel"}
@@ -52,7 +54,7 @@ class TestCartExpiry(CommonCase):
             self.assertEqual(self.sale.state, "cancel")
 
     def test_cart_expiry_delete(self):
-        so_date = fields.Datetime.from_string(self.sale.write_date)
+        so_date = fields.Datetime.from_string(self.so_date)
         today = fields.Datetime.to_string(so_date + timedelta(hours=5))
         self.backend.write(
             {"cart_expiry_delay": 1, "cart_expiry_policy": "delete"}
@@ -74,7 +76,7 @@ class TestCartExpiry(CommonCase):
         Ensure the cart is not deleted/canceled when the state is not draft.
         :return:
         """
-        so_date = fields.Datetime.from_string(self.sale.write_date)
+        so_date = fields.Datetime.from_string(self.so_date)
         today = fields.Datetime.to_string(so_date + timedelta(hours=5))
         self.sale.write({"state": "sent"})
         self.backend.write(

camptocamp/odoo-shopinvader-v1

shopinvader_cart_expiry/tests/test_cart_expiry.py
@@ -18,8 +18,9 @@ class TestCartExpiry(CartCase):
         super().setUp()
         self.sale_obj = self.env["sale.order"]
         self.partner = self.env.ref("shopinvader.partner_1")
-        self.sale = self.env.ref("shopinvader.sale_order_2")
         self.cart = self.env.ref("shopinvader.sale_order_2")
+        self.cart.write({"last_external_update_date": fields.Datetime.now()})
+        self.so_date = self.cart.last_external_update_date
         self.shopinvader_session = {"cart_id": self.cart.id}
         with self.work_on_services(
             partner=None, shopinvader_session=self.shopinvader_session
@@ -56,7 +57,7 @@ class TestCartExpiry(CartCase):
             )
 
     def test_cart_expiry_cancel(self):
-        so_date = fields.Datetime.from_string(self.sale.write_date)
+        so_date = fields.Datetime.from_string(self.so_date)
         today = fields.Datetime.to_string(so_date + timedelta(hours=5))
         self.backend.write({"cart_expiry_delay": 1, "cart_expiry_policy": "cancel"})
         now_method = "odoo.fields.Datetime.now"
@@ -71,7 +72,7 @@ class TestCartExpiry(CartCase):
             self.assertEqual(self.sale.state, "cancel")
 
     def test_cart_expiry_delete(self):
-        so_date = fields.Datetime.from_string(self.sale.write_date)
+        so_date = fields.Datetime.from_string(self.so_date)
         today = fields.Datetime.to_string(so_date + timedelta(hours=5))
         self.backend.write({"cart_expiry_delay": 1, "cart_expiry_policy": "delete"})
         now_method = "odoo.fields.Datetime.now"
@@ -122,7 +123,7 @@ class TestCartExpiry(CartCase):
         Ensure the cart is not deleted/canceled when the state is not draft.
         :return:
         """
-        so_date = fields.Datetime.from_string(self.sale.write_date)
+        so_date = fields.Datetime.from_string(self.so_date)
         today = fields.Datetime.to_string(so_date + timedelta(hours=5))
         self.sale.write({"state": "sent"})
         self.backend.write({"cart_expiry_delay": 1, "cart_expiry_policy": "cancel"})