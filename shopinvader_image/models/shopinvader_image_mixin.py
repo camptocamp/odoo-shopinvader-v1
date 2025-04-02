math0706/odoo-shopinvader

[FIX] computing image must be done in sudo
Indeed computing image can generate thumbnail and the api user may not have the access right for it

shopinvader_image/models/shopinvader_image_mixin.py
@@ -11,7 +11,9 @@ class ShopinvaderImageMixin(models.AbstractModel):
     _image_field = None
 
     images = fields.Serialized(
-        compute="_compute_image", string="Shopinvader Image"
+        compute="_compute_image",
+        string="Shopinvader Image",
+        compute_sudo=True,
     )
 
     def _compute_image(self):

camptocamp/odoo-shopinvader-v1

shopinvader_image/models/shopinvader_image_mixin.py
@@ -14,7 +14,11 @@ class ShopinvaderImageMixin(models.AbstractModel):
     _description = "Shopinvader Image Mixin"
     _image_field = None
 
-    images = Serialized(compute="_compute_images", string="Shopinvader Image")
+    images = Serialized(
+        compute="_compute_images",
+        string="Shopinvader Image",
+        compute_sudo=True,
+    )
     # Tech field to store images data.
     # It cannot be computed because the computation
     # might required generating thumbs