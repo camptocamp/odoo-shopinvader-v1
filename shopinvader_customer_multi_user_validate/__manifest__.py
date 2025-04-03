9b643b70aad42813cba19efc581011392ceb8f39
Simone Orsi <simahawk@gmail.com>
2025-04-03 14:27:50 +0200

    shopinvader_customer_multi_user_validate: migrate to v18

--- shopinvader_customer_multi_user_validate/__manifest__.py
+++ shopinvader_customer_multi_user_validate/__manifest__.py
@@ -7,9 +7,9 @@
     Glue module for
     `shopinvader_customer_validate` and `shopinvader_customer_multi_user`.
     """,
-    "version": "14.0.1.1.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Camptocamp SA",
+    "author": "Camptocamp",
     "website": "https://github.com/camptocamp/odoo-shopinvader-v1",
     "depends": ["shopinvader_customer_validate", "shopinvader_customer_multi_user"],
     "installable": True,
