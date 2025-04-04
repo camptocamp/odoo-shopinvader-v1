affcbf85aef4956d928c9d60fd4890d1c73994c2
Simone Orsi <simahawk@gmail.com>
2025-04-04 10:16:13 +0200

    shopinvader_product_media: migrate to v18

--- shopinvader_product_media/__manifest__.py
+++ shopinvader_product_media/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Shopinvader Product Media",
     "summary": "Index storage media data into external search engine",
-    "version": "14.0.1.1.0",
+    "version": "18.0.1.0.0",
     "category": "e-commerce",
     "website": "https://github.com/camptocamp/odoo-shopinvader-v1",
     "author": "Akretion, Camptocamp",
