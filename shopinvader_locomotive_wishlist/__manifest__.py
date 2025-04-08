73c2493f8c3a6a15fe2c64b78b90f7cb1763c19c
Simone Orsi <simahawk@gmail.com>
2025-04-08 19:36:15 +0200

    shopinvader_locomotive_wishlist: migrate to v18

--- shopinvader_locomotive_wishlist/__manifest__.py
+++ shopinvader_locomotive_wishlist/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Shopinvader Locomotive Wishlist",
     "summary": "Synchronize wishlist details to Locomotive users record.",
-    "version": "14.0.1.0.2",
+    "version": "18.0.1.0.0",
     "category": "e-commerce",
     "website": "https://github.com/camptocamp/odoo-shopinvader-v1",
     "author": "Camptocamp",
