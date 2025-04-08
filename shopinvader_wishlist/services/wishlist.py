camptocamp/odoo-shopinvader

s_wishlist: add new endpoint 'add_items_to_cart'
New endpoint to allow adding selected products to cart
instead of the whole set of products in the wishlist.

shopinvader_wishlist/services/wishlist.py
@@ -61,6 +61,16 @@ class WishlistService(Component):
         # return new cart
         return cart_service._to_json(cart)
 
+    def add_items_to_cart(self, _id, **params):
+        record = self._get(_id)
+        cart_service = self.component(usage="cart")
+        cart = cart_service._get()
+        prod_ids = [x["product_id"] for x in params["lines"]]
+        lines = record.get_lines_by_product(product_ids=prod_ids)
+        self._add_items_to_cart(record, cart, lines)
+        # return new cart
+        return cart_service._to_json(cart)
+
     def add_items(self, _id, **params):
         record = self._get(_id)
         self._add_items(record, params)
@@ -165,6 +175,28 @@ class WishlistService(Component):
             }
         }
 
+    def _validator_add_items_to_cart(self):
+        schema = self._validator_add_to_cart()
+        schema.update(
+            {
+                "lines": {
+                    "type": "list",
+                    "required": True,
+                    "schema": {
+                        "type": "dict",
+                        "schema": {
+                            "product_id": {
+                                "coerce": to_int,
+                                "required": True,
+                                "type": "integer",
+                            },
+                        },
+                    },
+                },
+            }
+        )
+        return schema
+
     def _validator_add_item(self):
         return self._validator_line_schema()
 
@@ -262,6 +294,11 @@ class WishlistService(Component):
 
     def _add_to_cart(self, record, cart):
         wizard = self._get_add_to_cart_wizard(record, cart)
+        return wizard.add_set()
+
+    def _add_items_to_cart(self, record, cart, lines):
+        wizard = self._get_add_to_cart_wizard(record, cart)
+        wizard.product_set_line_ids = lines
         return wizard.add_set()
 
     def _prepare_params(self, params, mode="create"):

camptocamp/odoo-shopinvader-v1

shopinvader_wishlist/services/wishlist.py
@@ -74,6 +74,16 @@ class WishlistService(Component):
         # return new cart
         return cart_service._to_json(cart)
 
+    def add_items_to_cart(self, _id, **params):
+        record = self._get(_id)
+        cart_service = self.component(usage="cart")
+        cart = cart_service._get()
+        prod_ids = [x["product_id"] for x in params["lines"]]
+        lines = record.get_lines_by_products(product_ids=prod_ids)
+        self._add_items_to_cart(record, cart, lines)
+        # return new cart
+        return cart_service._to_json(cart)
+
     def add_items(self, _id, **params):
         record = self._get(_id)
         self._add_items(record, params)
@@ -192,6 +202,28 @@ class WishlistService(Component):
             }
         }
 
+    def _validator_add_items_to_cart(self):
+        schema = self._validator_add_to_cart()
+        schema.update(
+            {
+                "lines": {
+                    "type": "list",
+                    "required": True,
+                    "schema": {
+                        "type": "dict",
+                        "schema": {
+                            "product_id": {
+                                "coerce": to_int,
+                                "required": True,
+                                "type": "integer",
+                            },
+                        },
+                    },
+                },
+            }
+        )
+        return schema
+
     def _validator_add_item(self):
         return self._validator_line_schema()
 
@@ -292,6 +324,11 @@ class WishlistService(Component):
 
     def _add_to_cart(self, record, cart):
         wizard = self._get_add_to_cart_wizard(record, cart)
+        return wizard.add_set()
+
+    def _add_items_to_cart(self, record, cart, lines):
+        wizard = self._get_add_to_cart_wizard(record, cart)
+        wizard.product_set_line_ids = lines
         return wizard.add_set()
 
     def _prepare_params(self, params, mode="create"):