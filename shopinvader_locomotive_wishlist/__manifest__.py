camptocamp/odoo-shopinvader

Add shopinvader_locomotive_wishlist

shopinvader_locomotive_wishlist/__manifest__.py
@@ -1,0 +1,17 @@
+# Copyright 2020 Camptocamp (http://www.camptocamp.com)
+# Simone Orsi <simahawk@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Shopinvader Locomotive Wishlist",
+    "summary": "Synchronize wishlist details to Locomotive users record.",
+    "version": "13.0.1.0.0",
+    "category": "e-commerce",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "author": "Camptocamp",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": False,
+    "auto_install": True,
+    "depends": ["shopinvader_locomotive", "shopinvader_wishlist"],
+}

camptocamp/odoo-shopinvader-v1

shopinvader_locomotive_wishlist/__manifest__.py
@@ -1,0 +1,17 @@
+# Copyright 2020 Camptocamp (http://www.camptocamp.com)
+# Simone Orsi <simahawk@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Shopinvader Locomotive Wishlist",
+    "summary": "Synchronize wishlist details to Locomotive users record.",
+    "version": "13.0.1.0.0",
+    "category": "e-commerce",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "author": "Camptocamp",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "auto_install": True,
+    "depends": ["shopinvader_locomotive", "shopinvader_wishlist"],
+}