camptocamp/odoo-shopinvader

Add shopinvader_sale_packaging_wishlist

shopinvader_sale_packaging_wishlist/services/wishlist.py
@@ -1,0 +1,51 @@
+# Copyright 2020 Camptocamp (http://www.camptocamp.com).
+# @author Simone Orsi <simahawk@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.addons.component.core import Component
+
+
+class WishlistService(Component):
+    _name = "shopinvader.wishlist.service"
+    _inherit = [
+        "shopinvader.wishlist.service",
+        "shopinvader.packaging.service.mixin",
+    ]
+
+    def _validator_line_schema(self):
+        res = super()._validator_line_schema()
+        res.update(self._validator_packaging_info())
+        return res
+
+    def _json_parser_line(self):
+        return super()._json_parser_line() + self._json_parser_line_packaging()
+
+    def _json_parser_line_packaging(self):
+        return [
+            (
+                "product_id:available_packaging",
+                self._json_parser_product_packaging,
+            ),
+            ("product_packaging_id:packaging", ("id", "name")),
+            "product_packaging_qty:packaging_qty",
+        ]
+
+    def _json_parser_product(self):
+        return super()._json_parser_product() + ["sell_only_by_packaging"]
+
+    def _json_parser_product_packaging(self, rec, fname):
+        return self._packaging_info_by_qty(rec.product_id, rec.quantity)
+
+    def _prepare_item(self, record, params):
+        res = super()._prepare_item(record, params)
+        pkg_params = self._packaging_values_from_params(params)
+        if pkg_params:
+            res.update(pkg_params)
+        return res
+
+    def _packaging_values_from_params(self, params):
+        res = super()._packaging_values_from_params(params)
+        if "product_packaging" in res:
+            # On sale.order.line they named a m2o field w/out `_id` :/
+            res["product_packaging_id"] = res.pop("product_packaging")
+        return res

shopinvader_sale_packaging_wishlist/tests/test_wishlist_packaging.py
@@ -1,0 +1,135 @@
+# Copyright 2020 Camptocamp (http://www.camptocamp.com).
+# @author Simone Orsi <simahawk@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo.addons.shopinvader_wishlist.tests.test_wishlist import (
+    CommonWishlistCase,
+)
+
+
+class WishlistCase(CommonWishlistCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.prod_set = cls.env.ref("shopinvader_wishlist.wishlist_1")
+        cls.prod_set.shopinvader_backend_id = cls.backend
+        cls.product_packaging = cls.env["product.packaging"].create(
+            {"name": "PKG TEST", "product_id": cls.prod1.id, "qty": 4}
+        )
+
+    def test_create(self):
+        params = dict(self.wl_params)
+        params["lines"][0].update(
+            {"packaging_id": self.product_packaging.id, "packaging_qty": 2}
+        )
+        res = self.wishlist_service.dispatch("create", params=params)["data"]
+        record = self.env["product.set"].browse(res["id"])
+        line = record.set_line_ids.filtered(
+            lambda x: x.product_id == self.prod1
+        )
+        self.assertEqual(line.product_packaging_id, self.product_packaging)
+        self.assertEqual(line.product_packaging_qty, 2)
+        self.assertEqual(line.quantity, 8)
+
+    def test_update_item1(self):
+        prod = self.env.ref("product.product_product_4b")
+        self.assertIn(prod, self.prod_set.mapped("set_line_ids.product_id"))
+        self._bind_products(prod)
+        # This line has no package
+        line = self.prod_set.get_line_by_product(product_id=prod.id)
+        self.assertEqual(line.quantity, 1)
+        # Add package and package qty
+        params = {
+            "lines": [
+                {
+                    "product_id": prod.id,
+                    "packaging_id": self.product_packaging.id,
+                    "packaging_qty": 4,
+                }
+            ]
+        }
+        self.wishlist_service.dispatch(
+            "update_items", self.prod_set.id, params=params
+        )
+        self.assertEqual(line.product_packaging_id, self.product_packaging)
+        self.assertEqual(line.product_packaging_qty, 4)
+        self.assertEqual(line.quantity, 16)
+        # Remove package
+        params = {
+            "lines": [
+                {
+                    "product_id": prod.id,
+                    "quantity": 7,
+                    "packaging_qty": 0,
+                    "packaging_id": None,
+                }
+            ]
+        }
+        self.wishlist_service.dispatch(
+            "update_items", self.prod_set.id, params=params
+        )
+        self.assertFalse(line.product_packaging_id)
+        self.assertEqual(line.product_packaging_qty, 0.0)
+        self.assertEqual(line.quantity, 7)
+
+    def test_jsonify(self):
+        res = self.wishlist_service._to_json_one(self.prod_set)
+        res_line = res["lines"][0]
+        prod = self.env.ref("product.product_product_4b")
+        packaging = self.env["product.packaging"].create(
+            {
+                "name": "PKG Foo",
+                "product_id": prod.id,
+                "qty": 100,
+                "can_be_sold": False,
+            }
+        )
+        self.assertEqual(res_line["quantity"], 1)
+        self.assertEqual(res_line["packaging_qty"], 0.0)
+        self.assertEqual(res_line["packaging"], None)
+        self.assertEqual(res_line["product"]["sell_only_by_packaging"], False)
+        self.assertEqual(
+            res_line["available_packaging"],
+            [
+                {
+                    "contained": None,
+                    "id": 1,
+                    "is_unit": True,
+                    "name": "Units",
+                    "qty": 1,
+                }
+            ],
+        )
+
+        # set some packaging values
+        packaging.can_be_sold = True
+        self.prod_set.set_line_ids[0].write(
+            {"product_packaging_qty": 3, "product_packaging_id": packaging.id}
+        )
+        res = self.wishlist_service._to_json_one(self.prod_set)
+        res_line = res["lines"][0]
+        self.assertEqual(res_line["quantity"], 300)
+        self.assertEqual(res_line["packaging_qty"], 3.0)
+        self.assertEqual(
+            res_line["packaging"], {"id": packaging.id, "name": packaging.name}
+        )
+        self.assertEqual(
+            res_line["available_packaging"],
+            [
+                {
+                    "contained": [
+                        {
+                            "id": prod.uom_id.id,
+                            "is_unit": True,
+                            "name": "Units",
+                            "qty": 100,
+                        }
+                    ],
+                    "id": packaging.id,
+                    "is_unit": False,
+                    "name": packaging.packaging_type_id.name,
+                    "qty": 3,
+                }
+            ],
+        )

camptocamp/odoo-shopinvader-v1

shopinvader_sale_packaging_wishlist/services/wishlist.py
@@ -1,0 +1,54 @@
+# Copyright 2020 Camptocamp (http://www.camptocamp.com).
+# @author Simone Orsi <simahawk@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.addons.component.core import Component
+
+
+class WishlistService(Component):
+    _name = "shopinvader.wishlist.service"
+    _inherit = [
+        "shopinvader.wishlist.service",
+        "shopinvader.packaging.service.mixin",
+    ]
+
+    def _validator_line_schema(self):
+        res = super()._validator_line_schema()
+        res.update(self._validator_packaging_info())
+        return res
+
+    def _json_parser_line(self):
+        return super()._json_parser_line() + self._json_parser_line_packaging()
+
+    def _json_parser_line_packaging(self):
+        return [
+            (
+                "product_id:packaging_by_qty",
+                self._json_parser_product_packaging,
+            ),
+            ("product_packaging_id:packaging", ("id", "name")),
+            "product_packaging_qty:packaging_qty",
+        ]
+
+    def _json_parser_product(self):
+        return super()._json_parser_product() + [
+            "packaging",
+            "sell_only_by_packaging",
+        ]
+
+    def _json_parser_product_packaging(self, rec, fname):
+        return self._packaging_info_by_qty(rec.product_id, rec.quantity)
+
+    def _prepare_item(self, record, params):
+        res = super()._prepare_item(record, params)
+        pkg_params = self._packaging_values_from_params(params)
+        if pkg_params:
+            res.update(pkg_params)
+        return res
+
+    def _packaging_values_from_params(self, params):
+        res = super()._packaging_values_from_params(params)
+        if "product_packaging" in res:
+            # On sale.order.line they named a m2o field w/out `_id` :/
+            res["product_packaging_id"] = res.pop("product_packaging")
+        return res

shopinvader_sale_packaging_wishlist/tests/test_wishlist_packaging.py
@@ -1,0 +1,135 @@
+# Copyright 2020 Camptocamp (http://www.camptocamp.com).
+# @author Simone Orsi <simahawk@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo.addons.shopinvader_wishlist.tests.test_wishlist import (
+    CommonWishlistCase,
+)
+
+
+class WishlistCase(CommonWishlistCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.prod_set = cls.env.ref("shopinvader_wishlist.wishlist_1")
+        cls.prod_set.shopinvader_backend_id = cls.backend
+        cls.product_packaging = cls.env["product.packaging"].create(
+            {"name": "PKG TEST", "product_id": cls.prod1.id, "qty": 4}
+        )
+
+    def test_create(self):
+        params = dict(self.wl_params)
+        params["lines"][0].update(
+            {"packaging_id": self.product_packaging.id, "packaging_qty": 2}
+        )
+        res = self.wishlist_service.dispatch("create", params=params)["data"]
+        record = self.env["product.set"].browse(res["id"])
+        line = record.set_line_ids.filtered(
+            lambda x: x.product_id == self.prod1
+        )
+        self.assertEqual(line.product_packaging_id, self.product_packaging)
+        self.assertEqual(line.product_packaging_qty, 2)
+        self.assertEqual(line.quantity, 8)
+
+    def test_update_item1(self):
+        prod = self.env.ref("product.product_product_4b")
+        self.assertIn(prod, self.prod_set.mapped("set_line_ids.product_id"))
+        self._bind_products(prod)
+        # This line has no package
+        line = self.prod_set.get_line_by_product(product_id=prod.id)
+        self.assertEqual(line.quantity, 1)
+        # Add package and package qty
+        params = {
+            "lines": [
+                {
+                    "product_id": prod.id,
+                    "packaging_id": self.product_packaging.id,
+                    "packaging_qty": 4,
+                }
+            ]
+        }
+        self.wishlist_service.dispatch(
+            "update_items", self.prod_set.id, params=params
+        )
+        self.assertEqual(line.product_packaging_id, self.product_packaging)
+        self.assertEqual(line.product_packaging_qty, 4)
+        self.assertEqual(line.quantity, 16)
+        # Remove package
+        params = {
+            "lines": [
+                {
+                    "product_id": prod.id,
+                    "quantity": 7,
+                    "packaging_qty": 0,
+                    "packaging_id": None,
+                }
+            ]
+        }
+        self.wishlist_service.dispatch(
+            "update_items", self.prod_set.id, params=params
+        )
+        self.assertFalse(line.product_packaging_id)
+        self.assertEqual(line.product_packaging_qty, 0.0)
+        self.assertEqual(line.quantity, 7)
+
+    def test_jsonify(self):
+        res = self.wishlist_service._to_json_one(self.prod_set)
+        res_line = res["lines"][0]
+        prod = self.env.ref("product.product_product_4b")
+        packaging = self.env["product.packaging"].create(
+            {
+                "name": "PKG Foo",
+                "product_id": prod.id,
+                "qty": 100,
+                "can_be_sold": False,
+            }
+        )
+        self.assertEqual(res_line["quantity"], 1)
+        self.assertEqual(res_line["packaging_qty"], 0.0)
+        self.assertEqual(res_line["packaging"], None)
+        self.assertEqual(res_line["product"]["sell_only_by_packaging"], False)
+        self.assertEqual(
+            res_line["packaging_by_qty"],
+            [
+                {
+                    "contained": None,
+                    "id": 1,
+                    "is_unit": True,
+                    "name": "Units",
+                    "qty": 1,
+                }
+            ],
+        )
+
+        # set some packaging values
+        packaging.can_be_sold = True
+        self.prod_set.set_line_ids[0].write(
+            {"product_packaging_qty": 3, "product_packaging_id": packaging.id}
+        )
+        res = self.wishlist_service._to_json_one(self.prod_set)
+        res_line = res["lines"][0]
+        self.assertEqual(res_line["quantity"], 300)
+        self.assertEqual(res_line["packaging_qty"], 3.0)
+        self.assertEqual(
+            res_line["packaging"], {"id": packaging.id, "name": packaging.name}
+        )
+        self.assertEqual(
+            res_line["packaging_by_qty"],
+            [
+                {
+                    "contained": [
+                        {
+                            "id": prod.uom_id.id,
+                            "is_unit": True,
+                            "name": "Units",
+                            "qty": 100,
+                        }
+                    ],
+                    "id": packaging.id,
+                    "is_unit": False,
+                    "name": packaging.packaging_type_id.name,
+                    "qty": 3,
+                }
+            ],
+        )