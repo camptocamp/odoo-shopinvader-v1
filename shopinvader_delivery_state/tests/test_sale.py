9d71dfb45c0ca99763c41a15138cb433e88674c6
Simone Orsi <simahawk@gmail.com>
2025-05-14 10:06:11 +0200

    shopinvader_delivery_state: migrate to v18

--- shopinvader_delivery_state/tests/test_sale.py
+++ shopinvader_delivery_state/tests/test_sale.py
@@ -33,10 +33,25 @@ class TestSalesService(CommonCase):
         states = sorted([x["state"] for x in res["data"]])
         self.assertEqual(states, ["shipping_unprocessed"] * len(res["data"]))
 
+    def _deliver_picking_partially(self, order):
+        """Validate only the first move of the order's picking."""
+        picking = order.picking_ids
+        first_move = picking.move_ids[0]
+        first_move.quantity = first_move.product_uom_qty
+        first_move.picked = True
+        picking.with_context(skip_backorder=True).button_validate()
+
+    def _deliver_picking_fully(self, order):
+        """Validate all moves of the order's picking."""
+        picking = order.picking_ids
+        for move in picking.move_ids:
+            move.quantity = move.product_uom_qty
+            move.picked = True
+        picking.button_validate()
+
     def test_state_partial(self):
         self.shop_sales.action_confirm()
-        line = self.shop_sales[0].order_line[0]
-        line.qty_delivered = line.product_uom_qty
+        self._deliver_picking_partially(self.shop_sales[0])
         service = self._get_service()
         res = service.search()
         states = sorted([x["state"] for x in res["data"]])
@@ -44,8 +59,7 @@ class TestSalesService(CommonCase):
 
     def test_state_done(self):
         self.shop_sales.action_confirm()
-        for line in self.shop_sales[0].order_line:
-            line.qty_delivered = line.product_uom_qty
+        self._deliver_picking_fully(self.shop_sales[0])
         service = self._get_service()
         res = service.search()
         states = sorted([x["state"] for x in res["data"]])
@@ -53,7 +67,6 @@ class TestSalesService(CommonCase):
 
     def test_state_done_not_shipped(self):
         self.shop_sales.action_confirm()
-        self.shop_sales[0].action_done()
         service = self._get_service()
         res = service.search()
         states = sorted([x["state"] for x in res["data"]])
